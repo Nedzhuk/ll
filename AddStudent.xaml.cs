4bd6059802be61a08203e168c67e66ee7e7c9cfa
Nedzhuk <jopkakota322@outlook.com>
2023-04-12 23:14:06 +0300

    student1

--- AddStudent.xaml.cs
+++ AddStudent.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ll
 {
     /// <summary>
-    /// Interaction logic for MainPage.xaml
+    /// Interaction logic for AddStudent.xaml
     /// </summary>
-    public partial class MainPage : Page
+    public partial class AddStudent : Window
     {
-        public MainPage()
+        public AddStudent()
         {
             InitializeComponent();
-        }
-
-        private void Student_Click(object sender, RoutedEventArgs e)
-        {
-            this.NavigationService.Navigate(new Students());
         }
     }
 }
--- Students.xaml.cs
+++ Students.xaml.cs
@@ -1,28 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ll
 {
-    /// <summary>
-    /// Interaction logic for Students.xaml
-    /// </summary>
     public partial class Students : Page
     {
-        public Students()
+        public struct StudentInfo
         {
+            public int Id { get; set; }
+            public string Surname { get; set; }
+            public string Name { get; set; }
+            public string Group { get; set; }
+            public string Phone { get; set; }
+            public string Adress { get; set; }
+        }
+        private readonly string _path = "Students.txt";
+        public Students() =>
             InitializeComponent();
+
+        private void Data_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> lines = File.ReadAllLines(_path).ToList();
+            List<StudentInfo> students = new();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string[] strings = lines[i].Split(".");
+                students.Add(new()
+                {
+                    Id = Convert.ToInt32(strings[0]),
+                    Surname = strings[1],
+                    Name = strings[2],
+                    Group = strings[3],
+                    Phone = strings[4],
+                    Adress = strings[5]
+                });
+            }
+            Data.ItemsSource = students;
+        }
+
+        private void Data_SizeChanged(object sender, SizeChangedEventArgs e)
+            => Data.MinColumnWidth = Data.ActualWidth / 6;
+
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            List<string> lines = File.ReadAllLines(_path).ToList();
+            List<StudentInfo> students = new();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string[] strings = lines[i].Split(".");
+                StudentInfo student = new()
+                {
+                    Id = Convert.ToInt32(strings[0]),
+                    Surname = strings[1],
+                    Name = strings[2],
+                    Group = strings[3],
+                    Phone = strings[4],
+                    Adress = strings[5]
+                };
+                if (student.Surname.ToLower().Contains(Search.Text.ToLower()))
+                    students.Add(student);
+            }
+            Data.ItemsSource = students;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            AddStudent window = new AddStudent();
+            window.Show();
         }
     }
 }
