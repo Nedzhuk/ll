6e95675dc13b06a912efcd43d4b3ec6c5fe4cd9c
Nedzhuk <jopkakota322@outlook.com>
2023-04-13 14:39:53 +0300

    c

--- AddStudent.xaml.cs
+++ AddStudent.xaml.cs
@@ -14,14 +14,42 @@ using System.Windows.Shapes;
 
 namespace ll
 {
-    /// <summary>
-    /// Interaction logic for AddStudent.xaml
-    /// </summary>
     public partial class AddStudent : Window
     {
         public AddStudent()
         {
             InitializeComponent();
         }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) =>
+            DragMove();
+
+        private void Search_TextChanged1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Search_TextChanged2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Search_TextChanged3(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Search_TextChanged4(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Search_TextChanged5(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) =>
+            Close();
     }
 }
